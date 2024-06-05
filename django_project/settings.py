3bf916eb233aa4418513f82bb08de5f8355d2eca
RYHarish <ryharishganesh@gmail.com>
2024-06-05 23:51:37 +0530

    forms, user accounts, static files

--- django_project/settings.py
+++ django_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "blog", 
+    "accounts"
 ]
 
 MIDDLEWARE = [
@@ -120,5 +121,10 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "static"] 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+STATIC_ROOT = BASE_DIR / "staticfiles" # new
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGIN_REDIRECT_URL = "home" 
+LOGOUT_REDIRECT_URL = "home" 
